0d844613c63500a3995f077e7182b5f53e886aa7
cphp <cphp@1a3d44eb-4cad-4980-a8ce-159a2d9775b0>
2009-09-20 20:42:25 +0000

    Homepage edited
    git-svn-id: http://svn.ideato.it/svn/pim/trunk@66 1a3d44eb-4cad-4980-a8ce-159a2d9775b0

--- apps/website/modules/content/templates/homepageSuccess.php
+++ apps/website/modules/content/templates/homepageSuccess.php
@@ -1,5 +1,12 @@
-<h2 id="slogan"><span></span>Simplify business management</h2>
+<h2 id="slogan"><span></span><?php echo __('Simplify business management')?></h2>
 
-            <p>La <strong>gestione amministrativa</strong> &egrave; un compito aziendale che richiede <strong>competenze</strong> e <strong>tempo</strong>.
-            Se sei un libero professionista o una piccola o media impresa il tuo <strong>tempo</strong> &egrave; <strong>denaro</strong>.</p>
-            <p>"<em><strong>Billify</strong> ti aiuta a risparmiare tempo e denaro semplificando i macchinosi compiti della burocrazia aziendale</em>".</p>
+<p><?php echo __('Business management is a task that requires business skills and a lot of time. 
+If you are a freelancer or a small or medium company your time is money.')?></p>
+
+<p><em><?php echo __('"Billify helps you save time and money by simplifying the cumbersome tasks of corporate bureaucracy."')?></em></p>
+		
+<!--
+<p>La <strong>gestione amministrativa</strong> &egrave; un compito aziendale che richiede <strong>competenze</strong> e <strong>tempo</strong>.
+Se sei un libero professionista o una piccola o media impresa il tuo <strong>tempo</strong> &egrave; <strong>denaro</strong>.</p>
+<p>"<em><strong>Billify</strong> ti aiuta a risparmiare tempo e denaro semplificando i macchinosi compiti della burocrazia aziendale</em>".</p>
+-->
--- apps/website/templates/website.php
+++ apps/website/templates/website.php
@@ -5,11 +5,8 @@
     <meta http-equiv="content-language" content="en" />
     <meta name="robots" content="all,follow" />
 
-    <meta name="author" lang="en" content="All: Your name [www.url.com]; e-mail: info@url.com" />
+    <meta name="author" lang="en" content="All: Francesco Trucchia [www.cphp.it]; e-mail: ft@ideato.it" />
     <meta name="copyright" lang="en" content="Webdesign: Nuvio [www.nuvio.cz]; e-mail: ahoj@nuvio.cz" />
-
-    <meta name="description" content="..." />
-    <meta name="keywords" content="..." />
 
     <!--[if lte IE 6]><link rel="stylesheet" type="text/css" href="css/impress/main-msie.css" /><![endif]-->
 
@@ -19,61 +16,40 @@
 <body>
 
 <div id="main">
-
-    <!-- Header -->
     <div id="header">
-
         <h1 id="logo"><a href="./" title="[Go to homepage]"><img src="/images/impress/logo.gif" alt="" /></a></h1>
         <hr class="noscreen" />
-
-        <!-- Navigation -->
         <div id="nav">
             <a href="/" id="nav-active">Home</a> <span>|</span>
             <a href="http://www.ideato.it/Azienda/Chi-siamo" target="_blank"><?php echo __('About')?></a> <span>|</span>
             <a href="<?php echo url_for('@content?page=support')?>"><?php echo __('Support')?></a> <span>|</span>
             <a href="http://www.ideato.it/Azienda/Contatti" target="_blank"><?php echo __('Contat us')?></a>
-        </div> <!-- /nav -->
-
-    </div> <!-- /header -->
-
-    <!-- Tray -->
+        </div>
+    </div>
+    
     <div id="tray">
-
         <ul>
-            <!--li id="tray-active"><a href="#">Homepage</a></li--> <!-- Active page -->
-            <!--li><a href="#">Tour</a></li-->
             <li><a href="/billify.php" target="_blank">Demo</a></li>
             <li><a href="<?php echo url_for('@download')?>">Download</a></li>
         </ul>
-
     <hr class="noscreen" />
-    </div> <!-- /tray -->
+    </div>
 
-    <!-- Promo -->
     <div id="col-top"></div>
     <div id="col" class="box">
-
-        <div id="ribbon"></div> <!-- /ribbon (design/ribbon.gif) -->
-
-        <!-- Screenshot in browser (replace tmp/browser.gif) -->
-        <div id="col-browser"><a href="#"><img src="tmp/browser.gif" width="255" height="177" alt="" /></a></div>
-
+        <div id="ribbon"></div>
+        <div id="col-browser"><img src="/images/screenshot/dashboard.jpg" width="255" height="177" alt="" /></div>
         <div id="col-text">
 	  <?php echo $sf_content; ?>
-        </div> <!-- /col-text -->
-
-    </div> <!-- /col -->
+        </div>
+    </div>
     <div id="col-bottom"></div>
 
     <hr class="noscreen" />
 
-    <!-- 3 columns -->
     <div id="cols3-top"></div>
     <div id="cols3" class="box">
-
-        <!-- Column I. -->
         <div class="col">
-
             <h3><a href="#"><?php echo __('Address book')?></a></h3>
 
             <!--p class="nom t-center"><a href="#"><img src="tmp/200x140.gif" alt="" /></a></p-->
@@ -91,80 +67,54 @@
             </div--> <!-- /col-text -->
 
             <!--div class="col-more"><a href="#"><img src="design/cols3-more.gif" alt="" /></a></div-->
-
-        </div> <!-- /col -->
+        </div>
 
         <hr class="noscreen" />
 
-        <!-- Column II. -->
         <div class="col">
-
             <h3><a href="#"><?php echo __('Payments and invoices')?></a></h3>
-
-            <!--p class="nom t-center"><a href="#"><img src="tmp/200x140.gif" alt="" /></a></p-->
-
-        </div> <!-- /col -->
+        </div>
 
         <hr class="noscreen" />
 
-        <!-- Column III. -->
         <div class="col last">
-
             <h3><a href="#"><?php echo __('Cash flow')?></a></h3>
-
             <!--p class="nom t-center"><a href="#"><img src="tmp/200x140.gif" alt="" /></a></p-->
-
-        </div> <!-- /col -->
-
+        </div>
         <hr class="noscreen" />
-
-    </div> <!-- /cols3 -->
+    </div>
     <div id="cols3-bottom"></div>
-
-    <!-- 2 columns -->
+<!--
     <div id="cols2-top"></div>
     <div id="cols2" class="box">
 
-        <!-- Blog -->
         <div id="col-left">
-
             <div class="title">
                 <span class="f-right"><a href="#" class="ico-rss">RSS feed</a></span>
                 <h4><?php echo __('Latest news')?></h4>
-            </div> <!-- /title -->
+            </div>
 
             <ul class="ul-list nomb">
                 <li><span class="f-right"><a href="#" class="ico-comment">1 commento</a></span> <span class="date">27.09.2008</span> <a href="#" class="article">L'amministrazione aziendale si fa Open Source</a></li>
                 <li><span class="f-right"><a href="#" class="ico-comment">0 commenti</a></span> <span class="date">27.09.2008</span> <a href="#" class="article">Completata la suite dei test automatici</a></li>
                 <li><span class="f-right"><a href="#" class="ico-comment">0 commenti</a></span> <span class="date">27.09.2008</span> <a href="#" class="article">Dalle ceneri di PIM Online nasce Billify</a></li>
             </ul>
-
-        </div> <!-- /col-left -->
+        </div>
 
         <hr class="noscreen" />
 
-        <!-- Testimonials -->
         <div id="col-right">
-
             <h4><span><?php echo __('From web')?></span></h4>
 
             <div class="box">
-
-                <!--div class="col-right-img"><img src="tmp/65x65.gif" width="65" height="65" alt="" /></div-->
-                <!--div class="col-right-text"-->
-
                     <p><em>Con Billify gestiamo tutta l'amministrazione dell'azienda. Con questo prodotto abbiamo sempre in tempo reale la situazione finanziare.</em></p>
                     <p class="high smaller">&ndash; <cite>Francesco Trucchia, ideato srl</cite></p>
+            </div>
+        </div>
 
-                <!--/div--> <!-- /col-right-text -->
-
-            </div> <!-- /box -->
-
-        </div> <!-- /col-right -->
-
-    </div> <!-- /cols2 -->
+    </div>
     <div id="cols2-bottom"></div>
-
+-->
     <hr class="noscreen" />
 
     <!-- Footer -->
@@ -176,9 +126,9 @@
 
         <p>Copyright &copy;&nbsp;2009 <strong><a href="#">ideato srl</a></strong>, All Rights Reserved &reg;</p>
 
-    </div> <!-- /footer -->
+    </div>
 
-</div> <!-- /main -->
+</div>
 
 </body>
 </html>
