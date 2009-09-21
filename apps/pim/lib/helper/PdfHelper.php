a35e01962c7b0aedeeab452384bb9f4ed5a657dc
cphp <cphp@1a3d44eb-4cad-4980-a8ce-159a2d9775b0>
2009-09-21 22:53:54 +0000

    Fix pdf bug
    git-svn-id: http://svn.ideato.it/svn/pim/trunk@69 1a3d44eb-4cad-4980-a8ce-159a2d9775b0

--- apps/pim/lib/helper/PdfHelper.php
+++ apps/pim/lib/helper/PdfHelper.php
@@ -2,7 +2,7 @@
  
 function pdf($html,$size,$orientation,$output_file)
 {
-	require_once(sfConfig::get('sf_app_lib_dir') .'/dompdf/dompdf_config.inc.php');
+	require_once(sfConfig::get('sf_lib_dir') .'/vendor/dompdf/dompdf_config.inc.php');
 	$dompdf = new DOMPDF();
 	$dompdf->set_paper('a4','portrait');
   	$dompdf->load_html($html);
