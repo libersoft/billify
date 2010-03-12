5bc2250a2063f73aa216023d12ae267b81a03e6c
cphp <cphp@1a3d44eb-4cad-4980-a8ce-159a2d9775b0>
2010-03-12 13:39:21 +0000

    Fix pdf include
    git-svn-id: http://svn.ideato.it/svn/pim/trunk@78 1a3d44eb-4cad-4980-a8ce-159a2d9775b0

--- apps/pim/lib/helper/PdfHelper.php
+++ apps/pim/lib/helper/PdfHelper.php
@@ -5,7 +5,7 @@ function pdf($html,$size,$orientation,$output_file)
 	require_once(sfConfig::get('sf_lib_dir') .'/vendor/dompdf/dompdf_config.inc.php');
 	$dompdf = new DOMPDF();
 	$dompdf->set_paper('a4','portrait');
-  	$dompdf->load_html($html);
+  $dompdf->load_html($html);
 	$dompdf->render();
  	$dompdf->stream("$output_file.pdf");
  	exit();
